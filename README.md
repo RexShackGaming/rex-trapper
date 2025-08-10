42beb487d1f571bbdbd2d7fda67dd9a23e984164
RexShack <rexshackuk@gmail.com>
2025-08-10 17:03:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# rex-trapper
+- discord : https://discord.gg/YUV7ebzkqs
+- youtube : https://www.youtube.com/@rexshack/videos
+
+# Dependancies
+- rsg-core
+- ox_lib
+
+# Installation
+- ensure that the dependancies are added and started
+- add rex-trapper to your resources folder
+- add items to your rsg-core\shared\items.lua
+- add images to your rsg-inventory\html\images
+
+# Starting the resource
+- add the following to your server.cfg file : ensure rex-trapper
+
+# support me
+- tebex : https://rexshackgaming.tebex.io/
---
+++ installation/shared_items.lua
@@ -0,0 +1,11 @@
+    -----------------------------------------------
+    -- YOUR CUSTOM ITEMS
+    -----------------------------------------------
+    legendary_pelt = { name = 'legendary_pelt', label = 'Legendary Pelt', weight = 100,  type = 'item', image = 'legendary_pelt.png', unique = false, useable = false, shouldClose = true, description = 'Legendary Pelt'},
+    perfect_pelt   = { name = 'perfect_pelt',   label = 'Perfect Pelt',   weight = 100,  type = 'item', image = 'perfect_pelt.png',   unique = false, useable = false, shouldClose = true, description = 'Perfect (* * *)'},
+    good_pelt      = { name = 'good_pelt',      label = 'Good Pelt',      weight = 100,  type = 'item', image = 'good_pelt.png',      unique = false, useable = false, shouldClose = true, description = 'Good (* *)'},
+    poor_pelt      = { name = 'poor_pelt',      label = 'Poor Pelt',      weight = 100,  type = 'item', image = 'poor_pelt.png',      unique = false, useable = false, shouldClose = true, description = 'Poor (*)'},
+    reptile_skin   = { name = 'reptile_skin',   label = 'Reptile Skin',   weight = 100,  type = 'item', image = 'reptile_skin.png',   unique = false, useable = false, shouldClose = true, description = 'Snake Skin'},
+    feather        = { name = 'feather',        label = 'Feather',        weight = 100,  type = 'item', image = 'feather.png',        unique = false, useable = false, shouldClose = true, description = 'Bird Feather'},
+    animal_heart   = { name = 'animal_heart',   label = 'Animal Heart',   weight = 100,  type = 'item', image = 'animal_heart.png',   unique = false, useable = false, shouldClose = true, description = 'Animal Heart'},
+    small_pelt     = { name = 'small_pelt',     label = 'Small Pelt',     weight = 100,  type = 'item', image = 'small_pelt.png',     unique = false, useable = false, shouldClose = true, description = 'Small Animal Pelt'},
